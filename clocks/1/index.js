c7bd18063cc9e95afb54bc2cfac0cde0497cb5b7
Tim <timothy.w.holman@gmail.com>
2017-03-12 20:06:25 -0400

    fully completed clock 1, running on regular time

--- clocks/1/index.js
+++ clocks/1/index.js
@@ -1,38 +1,21 @@
-var circle = document.querySelector('.clock');
-var body = document.body;
-
-var time = 0000;
-var hour = 0;
-var min = 0;
-
-var maxHours = 23;
-var maxMinutes = 59;
-
-function rotateTime() {
+(function clock() {
   
-  min++;
-  if( min > maxMinutes ) {
-    min = 0;
-    hour++;
+  var clockElement = document.querySelector('.clock');
+  var bodyElement = document.body;
+  var colorA = '#003973';
+  var colorB = '#ff6e7f';
+
+  function calculateGradient() {
+
+    var date = new Date();
+    var percentage = (date.getHours() / 24 + date.getMinutes() / (60 * 24));
+    var degree = Math.floor(percentage * 360);
+
+    bodyElement.style.background = 'linear-gradient(' + degree + 'deg, ' + colorA + ',' + colorB + ')';
+    clockElement.style.background = 'linear-gradient(' + (degree - 180) + 'deg, ' + colorA + ',' + colorB + ')';
   }
-  
-  if( hour > maxHours ) {
-    hour = 0;
-  }
-}
 
-function loop() {
-  rotateTime();
-  updateGradient();
-  requestAnimationFrame(loop);
-}
+  setInterval(calculateGradient, 1000);
+  calculateGradient();
 
-function updateGradient() {
-  var percentage = (hour / 24 + min / (60 * 24));
-  var degree = Math.floor(percentage * 360);
-  
-  body.style.background = "linear-gradient(" + degree + "deg, #ff6e7f, #003973)";
-  circle.style.background = "linear-gradient(" + (degree - 180) + "deg, #ff6e7f, #003973)"
-}
-
-loop();
+})();
